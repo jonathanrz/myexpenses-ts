a57c9752af6841e0f0b6fae9b6defee7b523c0f8
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-06-05 20:51:21 -0300

    scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+  Redirect,
+} from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+
+let isAuthenticated = false;
+
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
+
+interface PrivateRouteProps {
+  children: ReactNode;
+  exact: boolean | undefined;
+  path: string | string[] | undefined;
+}
+
+function PrivateRoute({ children, exact, path }: PrivateRouteProps) {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <PrivateRoute exact path="/">
+          <HomePage />
+        </PrivateRoute>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
