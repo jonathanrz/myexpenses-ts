d9f7c58ec7ec8c9b750bbf276222f4473743ab61
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-12-23 18:37:07 -0300

    categories queries

--- src/pages/Categories/List.tsx
+++ src/pages/Categories/List.tsx
@@ -10,11 +10,10 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import useAxios from "../../hooks/useAxios";
-import useAsync from "../../hooks/useAsync";
 import Form from "./Form";
 import CategoryRow from "./CategoryRow";
-import { Category } from "./model";
+import { Category } from "../../models/Category";
+import useCategoriesQuery from "../../queries/categories";
 
 const useStyles = makeStyles({
   container: {
@@ -29,20 +28,15 @@ const useStyles = makeStyles({
 
 function CategoriesList() {
   const classes = useStyles();
+  const { query, deleteMutation } = useCategoriesQuery();
 
-  const axios = useAxios();
-  const dataAsync = useAsync(() => {
-    return axios.get("categories").then(({ data }) => data.data);
-  });
-
-  if (dataAsync.pending) return <CircularProgress />;
-  if (dataAsync.error) return <Alert severity="error">{dataAsync.error}</Alert>;
-
-  const onCategorySaved = () => dataAsync.execute();
+  if (query.isLoading) return <CircularProgress />;
+  if (query.isError)
+    return <Alert severity="error">{query.error.message}</Alert>;
 
   function deleteCategory(id: string) {
     if (window.confirm("Delete?")) {
-      axios.delete(`categories/${id}`).then(() => dataAsync.execute());
+      deleteMutation.mutate(id);
     }
   }
 
@@ -57,15 +51,14 @@ function CategoriesList() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {dataAsync.result.map((category: Category) => (
-              <CategoryRow
-                key={category.id}
-                category={category}
-                axios={axios}
-                onCategorySaved={onCategorySaved}
-                deleteCategory={deleteCategory}
-              />
-            ))}
+            {query.data &&
+              query.data.map((category: Category) => (
+                <CategoryRow
+                  key={category.id}
+                  category={category}
+                  deleteCategory={deleteCategory}
+                />
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -73,7 +66,7 @@ function CategoriesList() {
         <Typography component="h1" variant="h5">
           New Category
         </Typography>
-        <Form axios={axios} onCategorySaved={onCategorySaved} />
+        <Form />
       </Paper>
     </div>
   );
---
+++ src/queries/categories.tsx
@@ -0,0 +1,46 @@
+import useAxios from "../hooks/useAxios";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { Category } from "../models/Category";
+
+const MODEL_NAME = "category";
+const PATH = "categories";
+
+function useCategoriesQuery() {
+  const queryClient = useQueryClient();
+  const axios = useAxios();
+
+  const query = useQuery<Array<Category>, Error>(PATH, () =>
+    axios.get(PATH).then(({ data }) => data.data)
+  );
+
+  const mutation = useMutation<Category, Error, Category>(
+    (values) => {
+      if (values.id) {
+        return axios
+          .patch(`/${PATH}/${values.id}`, { [MODEL_NAME]: values })
+          .then(({ data }) => data.data);
+      }
+      return axios
+        .post(`/${PATH}`, { [MODEL_NAME]: values })
+        .then(({ data }) => data.data);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(PATH);
+      },
+    }
+  );
+
+  const deleteMutation = useMutation<void, Error, String>(
+    (id) => axios.delete(`${PATH}/${id}`),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(PATH);
+      },
+    }
+  );
+
+  return { query, mutation, deleteMutation };
+}
+
+export default useCategoriesQuery;
