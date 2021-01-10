6a93f0189a2014a3a346a29cac3553bdb2d3fdac
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-01-10 17:37:10 -0300

    render confirm button

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,9 +1,12 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import moment, { Moment } from "moment";
 import sortBy from "lodash/sortBy";
 import { makeStyles } from "@material-ui/core/styles";
 import Alert from "@material-ui/lab/Alert";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import IconButton from "@material-ui/core/IconButton";
+import ClearIcon from "@material-ui/icons/Clear";
+import DoneIcon from "@material-ui/icons/Done";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -13,6 +16,8 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Currency from "helpers/currency";
 import { Account } from "models/Account";
+import { Expense } from "models/Expense";
+import { Receipt } from "models/Receipt";
 import PrivatePage from "components/PrivatePage";
 import ValueCell from "components/shared/ValueCell";
 import useAccountsQuery from "queries/accounts";
@@ -26,18 +31,30 @@ interface Transaction {
   account?: Account;
   date: Moment;
   value: number;
-  receipt?: boolean;
-  expense?: boolean;
+  receipt?: Receipt;
+  expense?: Expense;
 }
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  valueCell: {
+    alignItems: "center",
+    display: "flex",
+  },
+});
 
 function Home() {
   const classes = useStyles();
+  const [confirming, setConfirming] = useState(false);
   const month = moment();
   const { query: accountsQuery } = useAccountsQuery();
-  const { query: receiptsQuery } = useReceiptsQuery(month);
-  const { monthQuery: expensesQuery } = useExpensesQuery(month);
+  const {
+    query: receiptsQuery,
+    confirmMutation: confirmReceipt,
+  } = useReceiptsQuery(month);
+  const {
+    monthQuery: expensesQuery,
+    confirmMutation: confirmExpense,
+  } = useExpensesQuery(month);
 
   const transactions = useMemo(() => {
     const transactions: Transaction[] = [
@@ -48,7 +65,7 @@ function Home() {
         account: receipt.account,
         date: receipt.date,
         value: receipt.value,
-        receipt: true,
+        receipt,
       })) || []),
       ...(expensesQuery.data?.map((expense) => ({
         id: `expense_${expense.id}`,
@@ -59,12 +76,40 @@ function Home() {
         account: expense.account || expense.credit_card?.account,
         date: expense.date,
         value: expense.value,
-        expense: true,
+        expense,
       })) || []),
     ];
 
     return sortBy(transactions, "date");
   }, [receiptsQuery.data, expensesQuery.data]);
+
+  function confirm(transaction: Transaction) {
+    setConfirming(true);
+
+    let mutation;
+
+    if (transaction.receipt) {
+      mutation = confirmReceipt.mutateAsync(transaction.receipt);
+    } else if (transaction.expense) {
+      mutation = confirmExpense.mutateAsync(transaction.expense);
+    }
+
+    mutation?.finally(() => setConfirming(false));
+  }
+
+  function renderConfirmButton(transaction: Transaction) {
+    if (confirming) return <CircularProgress />;
+
+    return (
+      <IconButton
+        color="default"
+        size="small"
+        onClick={() => confirm(transaction)}
+      >
+        {transaction.confirmed ? <ClearIcon /> : <DoneIcon />}
+      </IconButton>
+    );
+  }
 
   function renderEmptyRow() {
     return (
@@ -115,12 +160,15 @@ function Home() {
                 {accountsQuery.data?.map((account) => (
                   <TableCell key={account.id}>
                     {account.id === transaction.account?.id ? (
-                      <ValueCell
-                        value={transaction.value}
-                        confirmed={transaction.confirmed}
-                        receipt={transaction.receipt}
-                        expense={transaction.expense}
-                      />
+                      <div className={classes.valueCell}>
+                        <ValueCell
+                          value={transaction.value}
+                          confirmed={transaction.confirmed}
+                          receipt={Boolean(transaction.receipt)}
+                          expense={Boolean(transaction.expense)}
+                        />
+                        {renderConfirmButton(transaction)}
+                      </div>
                     ) : (
                       ""
                     )}
