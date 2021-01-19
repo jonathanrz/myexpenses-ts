1db5db58ddeffe24be1d1ce3aff8d3ed88349f39
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-01-19 20:43:57 -0300

    filter out old nubank events

--- src/pages/Nubank/index.tsx
+++ src/pages/Nubank/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 import { makeStyles } from "@material-ui/core/styles";
 import PrivatePage from "components/PrivatePage";
 import Button from "@material-ui/core/Button";
@@ -11,6 +12,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Currency from "helpers/currency";
+import { NubankEvent } from "models/NubankEvent";
 
 const useStyles = makeStyles({
   container: {
@@ -21,14 +23,14 @@ const useStyles = makeStyles({
   },
 });
 
+const today = moment();
+
 function NubankPage() {
   const classes = useStyles();
   const [url, setUrl] = useState("");
   const [token, setToken] = useState("");
   const [importingData, setImportingData] = useState(false);
-  const [events, setEvents] = useState([]);
-
-  console.log({ events });
+  const [events, setEvents] = useState<NubankEvent[]>([]);
 
   useEffect(() => {
     setUrl(localStorage.getItem("import-data-url") || "");
@@ -47,9 +49,13 @@ function NubankPage() {
       },
     })
       .then((res) => res.json())
-      .then(
-        ({ events }) =>
-          setEvents(events.filter((e: any) => e.category === "transaction")) //TODO: create nubank event type
+      .then(({ events }) =>
+        setEvents(
+          events
+            .filter((e: NubankEvent) => e.category === "transaction")
+            .map((e: NubankEvent) => ({ ...e, time: moment(e.time) }))
+            .filter((e: NubankEvent) => today.diff(e.time, "months") < 13)
+        )
       )
       .finally(() => setImportingData(false));
   }
@@ -86,20 +92,20 @@ function NubankPage() {
             <TableHead>
               <TableRow>
                 <TableCell>ID</TableCell>
+                <TableCell>Date</TableCell>
                 <TableCell>Description</TableCell>
                 <TableCell align="right">Installments</TableCell>
                 <TableCell align="right">Amount</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {events.map((
-                e: any //TODO: create nubank event type
-              ) => (
+              {events.map((e) => (
                 <TableRow key={e.id}>
                   <TableCell>{e.id}</TableCell>
+                  <TableCell>{e.time.format("lll")}</TableCell>
                   <TableCell>{e.description}</TableCell>
                   <TableCell align="right">
-                    {e.details?.charges?.amount || 0}
+                    {e.details?.charges?.count || 0}
                   </TableCell>
                   <TableCell align="right">
                     {Currency.format(e.amount)}{" "}
