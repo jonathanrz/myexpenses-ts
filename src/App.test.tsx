069c620c60548b4b2126157179d6acba8628f1c1
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-03-29 20:33:32 -0300

    fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders sign up form", () => {
+  const { getByLabelText } = render(<App />);
+  const emailInput = getByLabelText("Email *");
+  expect(emailInput).toBeInTheDocument();
+  const passwordInput = getByLabelText("Password *");
+  expect(passwordInput).toBeInTheDocument();
 });
