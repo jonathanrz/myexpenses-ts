a753e20aeaa0f5b59d8c45567f8ac7de36e9f7ee
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-08-21 15:49:51 -0300

    extract useMonthExpenseQuery

---
+++ src/hooks/useMonthExpenseQuery.tsx
@@ -0,0 +1,100 @@
+import { useMemo } from "react";
+import { Moment } from "moment";
+import findIndex from "lodash/findIndex";
+import groupBy from "lodash/groupBy";
+import sortBy from "lodash/sortBy";
+import orderBy from "lodash/orderBy";
+import sumBy from "lodash/sumBy";
+import useExpensesQuery from "queries/expenses";
+import useBillsQuery from "queries/bills";
+import useCategoriesQuery from "queries/categories";
+import { CategoryData } from "models/CategoryData";
+import { CategoryDataItem } from "models/CategoryDataItem";
+
+function useMonthExpenseQuery(month: Moment) {
+  const { query: expensesQuery } = useExpensesQuery(month);
+  const { monthQuery: billsQuery } = useBillsQuery(month);
+  const { query: categoriesQuery } = useCategoriesQuery();
+
+  const expensesMappedData = useMemo(() => {
+    if (!expensesQuery.data) return [];
+
+    return expensesQuery.data.map((expense) => ({
+      id: `expense_${expense.id}`,
+      date: expense.date,
+      name: expense.name,
+      categoryName: expense.category?.name,
+      category: expense.category,
+      expense: expense,
+      placeName: expense.place?.name,
+      place: expense.place,
+      value: expense.value,
+    })) as Array<CategoryDataItem>;
+  }, [expensesQuery.data]);
+
+  const billsMappedData = useMemo(() => {
+    if (!billsQuery.data) return [];
+
+    return billsQuery.data.map((bill) => ({
+      id: `bill_${bill.id}`,
+      name: `${bill.name} (bill)`,
+      categoryName: bill.category?.name,
+      category: bill.category,
+      placeName: "No Place",
+      value: bill.value,
+    })) as Array<CategoryDataItem>;
+  }, [billsQuery.data]);
+
+  const groupedByCategory = useMemo(() => {
+    const data = [...expensesMappedData, ...billsMappedData];
+    const grouped = groupBy(
+      data.filter(
+        (e) =>
+          (!e.category || e.category?.display_in_month_expense) &&
+          !(e.name.startsWith("Fatura") || e.name.startsWith("invoice"))
+      ) || [],
+      "categoryName"
+    );
+
+    let result: Array<CategoryData> = [];
+
+    Object.values(grouped).forEach((value: Array<CategoryDataItem>) => {
+      result.push({
+        categoryName: value[0].category?.name || "Unnamed",
+        category: value[0].category,
+        items: orderBy(value, ["placeName", "date"]),
+        value: sumBy(value, "value"),
+        forecast: value[0].category?.forecast || 0,
+      });
+    });
+
+    if (categoriesQuery.data) {
+      categoriesQuery.data
+        .filter(
+          (cat) =>
+            cat.display_in_month_expense &&
+            findIndex(result, (r) => r.category?.id === cat.id) === -1
+        )
+        .forEach((cat) => {
+          result.push({
+            categoryName: cat.name,
+            category: cat,
+            items: [],
+            value: 0,
+            forecast: cat.forecast || 0,
+          });
+        });
+    }
+
+    return sortBy(result, "categoryName");
+  }, [expensesMappedData, billsMappedData, categoriesQuery.data]);
+
+  return {
+    isLoading: expensesQuery.isLoading,
+    isError: expensesQuery.isError,
+    error: expensesQuery.error,
+    data: groupedByCategory,
+  };
+}
+
+export default useMonthExpenseQuery;
--- src/pages/MonthExpenses/index.tsx
+++ src/pages/MonthExpenses/index.tsx
@@ -1,9 +1,5 @@
 import React, { useMemo, useState } from "react";
 import moment, { Moment } from "moment";
-import findIndex from "lodash/findIndex";
-import groupBy from "lodash/groupBy";
-import sortBy from "lodash/sortBy";
-import orderBy from "lodash/orderBy";
 import sumBy from "lodash/sumBy";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -19,10 +15,9 @@ import Paper from "@material-ui/core/Paper";
 import PrivatePage from "components/PrivatePage";
 import MonthTabs from "components/MonthTabs";
 import Currency from "helpers/currency";
-import useMonthExpenseQuery from "./useMonthExpenseQuery";
-import useCategoriesQuery from "queries/categories";
+import useMonthExpenseQuery from "hooks/useMonthExpenseQuery";
+import { CategoryData } from "models/CategoryData";
 import MonthExpensesRow from "./MonthExpensesRow";
-import { CategoryData, CategoryDataItem } from "./models";
 import OverCell from "./OverCell";
 
 const today = moment();
@@ -38,62 +33,10 @@ function MonthExpenses() {
   const classes = useStyles();
   const [currentMonth, setCurrentMonth] = useState(today);
   const query = useMonthExpenseQuery(currentMonth);
-  const { query: categoriesQuery } = useCategoriesQuery();
-
-  const groupedByCategory = useMemo(() => {
-    const grouped = groupBy(
-      query.data?.filter(
-        (e) =>
-          (!e.category || e.category?.display_in_month_expense) &&
-          ![
-            "Fatura Nubank",
-            "Fatura C6",
-            "invoice Nubank",
-            "invoice C6",
-          ].includes(e.name)
-      ) || [],
-      "categoryName"
-    );
-
-    let result: Array<CategoryData> = [];
-
-    Object.values(grouped).forEach((value: Array<CategoryDataItem>) => {
-      result.push({
-        categoryName: value[0].category?.name || "Unnamed",
-        category: value[0].category,
-        items: orderBy(value, ["placeName", "date"]),
-        value: sumBy(value, "value"),
-        forecast: value[0].category?.forecast || 0,
-      });
-    });
-
-    if (categoriesQuery.data) {
-      categoriesQuery.data
-        .filter(
-          (cat) =>
-            cat.display_in_month_expense &&
-            findIndex(result, (r) => r.category?.id === cat.id) === -1
-        )
-        .forEach((cat) => {
-          result.push({
-            categoryName: cat.name,
-            category: cat,
-            items: [],
-            value: 0,
-            forecast: cat.forecast || 0,
-          });
-        });
-    }
-
-    return sortBy(result, "categoryName");
-  }, [query.data, categoriesQuery.data]);
 
   const [totalValue, totalForecast] = useMemo(
-    () => [
-      sumBy(groupedByCategory, "value"),
-      sumBy(groupedByCategory, "forecast"),
-    ],
-    [groupedByCategory]
+    () => [sumBy(query.data, "value"), sumBy(query.data, "forecast")],
+    [query.data]
   );
 
   function handleMonthSelected(event: React.ChangeEvent<{}>, newMonth: Moment) {
@@ -121,7 +64,7 @@ function MonthExpenses() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {groupedByCategory.map((cat) => (
+            {query.data?.map((cat: CategoryData) => (
               <MonthExpensesRow
                 key={cat.category?.name || "unnamed"}
                 catData={cat}
